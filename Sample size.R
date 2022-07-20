e3ffaba105ef601d71625aa91e2002f797156c40
Chao-lilly <wang_chaochen@lilly.com>
2022-07-20 20:55:58 +0900

    updated sample size calculation

---
+++ Sample size.R
@@ -0,0 +1,207 @@
+#https://statsdirect.com/help/sample_size/matched_case_control.htm
+
+
+library(epiR)
+
+##%######################################################%##
+#                                                          #
+####                     p0 = 0.03                      ####
+#                                                          #
+##%######################################################%##
+
+
+epi.sscc(OR = 2.2, # minimum OR
+         p1 = NA,
+         p0 = 0.03, # exposure % among control
+         n = NA, 
+         power = 0.90, # (1 - beta)
+         r = 1, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "matched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
+
+
+epi.sscc(OR = 2.2, # minimum OR
+         p1 = NA,
+         p0 = 0.03, # exposure % among control
+         n = NA, 
+         power = 0.90, # (1 - beta)
+         r = 2, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "matched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
+
+
+epi.sscc(OR = 2.2, # minimum OR
+         p1 = NA,
+         p0 = 0.03, # exposure % among control
+         n = NA, 
+         power = 0.90, # (1 - beta)
+         r = 3, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "matched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
+##%######################################################%##
+#                                                          #
+####                     p0 = 0.05                      ####
+#                                                          #
+##%######################################################%##
+
+
+epi.sscc(OR = 2.2, # minimum OR
+         p1 = NA,
+         p0 = 0.05, # exposure % among control
+         n = NA, 
+         power = 0.90, # (1 - beta)
+         r = 1, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "matched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
+
+
+epi.sscc(OR = 2.2, # minimum OR
+         p1 = NA,
+         p0 = 0.05, # exposure % among control
+         n = NA, 
+         power = 0.90, # (1 - beta)
+         r = 2, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "matched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
+
+
+epi.sscc(OR = 2.2, # minimum OR
+         p1 = NA,
+         p0 = 0.05, # exposure % among control
+         n = NA, 
+         power = 0.90, # (1 - beta)
+         r = 3, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "matched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
+
+##%######################################################%##
+#                                                          #
+####                     p0 = 0.08                      ####
+#                                                          #
+##%######################################################%##
+
+epi.sscc(OR = 2.2, # minimum OR
+         p1 = NA,
+         p0 = 0.08, # exposure % among control
+         n = NA, 
+         power = 0.90, # (1 - beta)
+         r = 1, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "matched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
+
+
+epi.sscc(OR = 2.2, # minimum OR
+         p1 = NA,
+         p0 = 0.08, # exposure % among control
+         n = NA, 
+         power = 0.90, # (1 - beta)
+         r = 2, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "matched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
+
+
+epi.sscc(OR = 2.2, # minimum OR
+         p1 = NA,
+         p0 = 0.08, # exposure % among control
+         n = NA, 
+         power = 0.90, # (1 - beta)
+         r = 3, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "matched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
+
+
+##%######################################################%##
+#                                                          #
+####                     p0 = 0.10                      ####
+#                                                          #
+##%######################################################%##
+
+epi.sscc(OR = 2.2, # minimum OR
+         p1 = NA,
+         p0 = 0.10, # exposure % among control
+         n = NA, 
+         power = 0.90, # (1 - beta)
+         r = 1, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "matched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
+
+
+epi.sscc(OR = 2.2, # minimum OR
+         p1 = NA,
+         p0 = 0.10, # exposure % among control
+         n = NA, 
+         power = 0.90, # (1 - beta)
+         r = 2, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "matched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
+
+
+epi.sscc(OR = 2.2, # minimum OR
+         p1 = NA,
+         p0 = 0.10, # exposure % among control
+         n = NA, 
+         power = 0.90, # (1 - beta)
+         r = 3, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "matched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
+
