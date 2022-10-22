ac1c2cac3924856628c17ecdb4766bf3906ecd9e
Chao-lilly <wang_chaochen@lilly.com>
2022-10-22 11:31:08 +0900

    met Oura-san with Lilly Rstudio

---
+++ SampleSizeLp(a).R
@@ -0,0 +1,39 @@
+#https://statsdirect.com/help/sample_size/matched_case_control.htm
+
+
+library(epiR)
+
+##%######################################################%##
+#                                                          #
+####                     p0 = 0.05                      ####
+#                                                          #
+##%######################################################%##
+
+
+epi.sscc(OR = 1.5, # minimum OR
+         p1 = NA,
+         p0 = 0.05, # exposure % among control
+         n = NA, 
+         power = 0.80, # (1 - beta)
+         r = 1, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "matched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
+
+epi.sscc(OR = 1.5, # minimum OR
+         p1 = NA,
+         p0 = 0.05, # exposure % among control
+         n = NA, 
+         power = 0.80, # (1 - beta)
+         r = 1, 
+         phi.coef = 0, 
+         design = 1, 
+         sided.test = 2, 
+         conf.level = 0.95, 
+         method = "unmatched", 
+         nfractional = FALSE, 
+         fleiss = FALSE)
